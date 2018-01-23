f2b4e400dff3a6bdbda2e561b8e4785a149af288
beffery <bfding@cloume.com>
2018-01-23 12:55:03 +0800

    a test commit

--- firstClass.java
+++ firstClass.java
@@ -1,5 +1,6 @@
 public class firstClass {
     public static void main(String []args){
         System.out.println("this is a first java class!!!");
+        System.out.println("this is a first java class!!!");
     }
 }
